3e0f82f2e381e212d87f1ee3a2d387a6f01568b8
Rodrigo Gallert <rodrigo.gallert96@gmail.com>
2024-10-25 01:25:54 -0300

    added base repository

---
+++ Backend/Suppliers.Data/Repositories/Repository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Suppliers.Domain.Entities;
+using Suppliers.Domain.Interfaces.Repositories;
+
+namespace Suppliers.Data.Repositories;
+public class Repository<T> : IRepository<T> where T : BaseEntity
+{
+    protected readonly SuppliersContext _context;
+
+    public Repository(SuppliersContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<T> AddAsync(T entity)
+    {
+        var obj = await _context
+                .Set<T>()
+                .AddAsync(entity);
+        await _context.SaveChangesAsync();
+
+        return obj.Entity;
+    }
+
+    public async Task DeleteAsync(T entity)
+    {
+        entity.IsActive = false;
+        await UpdateAsync(entity);
+    }
+
+    public async Task<T> GetAsync(ulong id)
+    {
+        return await _context
+                .Set<T>()
+                .FindAsync(id);
+    }
+
+    public async Task<ICollection<T>> GetAsync()
+    {
+        return await _context
+            .Set<T>()
+            .Where(x => x.IsActive)
+            .ToListAsync();
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        entity.UpdatedAt = DateTime.Now;
+        var obj = _context
+            .Set<T>()
+            .Update(entity);
+    }
+}
---
+++ Backend/Suppliers.Domain/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using Suppliers.Domain.Entities;
+
+namespace Suppliers.Domain.Interfaces.Repositories;
+public interface IRepository<T> where T : BaseEntity
+{
+    Task<T> GetAsync(ulong id);
+    Task<ICollection<T>> GetAsync();
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+}
